ea9dae82a6d0b2cd23e0c122c84d669e5806c829
Lynchy <dan@lynchy.co.nz>
2020-05-21 18:56:14 +1000

    add encrypt script

--- decrypt_schema.sh
+++ decrypt_schema.sh
@@ -1,11 +1,11 @@
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_SCHEMA_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
 --output backend/db/init/00-database.sql backend/db/init/00-database.sql.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_SCHEMA_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
 --output backend/db/init/01-functions-1.sql backend/db/init/01-functions-1.sql.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_SCHEMA_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
 --output backend/db/init/02-functions-2.sql backend/db/init/02-functions-2.sql.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_SCHEMA_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
 --output backend/db/init/03-policies.sql backend/db/init/03-policies.sql.gpg
---
+++ encrypt_schema.sh
@@ -0,0 +1,4 @@
+gpg --symmetric --cipher-algo AES256 backend/db/init/00-database.sql
+gpg --symmetric --cipher-algo AES256 backend/db/init/01-functions-1.sql
+gpg --symmetric --cipher-algo AES256 backend/db/init/02-functions-2.sql
+gpg --symmetric --cipher-algo AES256 backend/db/init/03-policies.sql
