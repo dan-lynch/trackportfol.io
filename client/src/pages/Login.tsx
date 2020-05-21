2b0334ce4334688a0477ff7ae82f56f6a8ea9e0f
Lynchy <dan@lynchy.co.nz>
2020-05-21 21:40:39 +1000

    test actions

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Dashboard: React.FC<Props> = () => {
   return (
     <React.Fragment>
-      <h2>Login</h2>
+      <h2>LOGIN</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Login />
       </Suspense>
