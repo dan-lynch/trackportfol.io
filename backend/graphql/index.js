c355407713e32076cecc89c22d386373dbef7d71
Lynchy <dan@lynchy.co.nz>
2020-05-24 18:04:37 +1000

    use express, configure postgraphile, update dockerfile

---
+++ backend/graphql/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const compression = require('compression')
+const helmet = require('helmet')
+const { postgraphile } = require("postgraphile")
+
+const app = express()
+
+app.use(helmet())
+
+app.use(compression())
+
+app.use(
+  postgraphile(
+    process.env.DATABASE_URL,
+    "public",
+    {
+    subscriptions: true,
+    retryOnInitFail: true,
+    dynamicJson: true,
+    setofFunctionsContainNulls: false,
+    ignoreRBAC: false,
+    ignoreIndexes: false,
+    graphiql: true,
+    enhanceGraphiql: true,
+    pgDefaultRole: process.env.DEFAULT_ROLE,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtPgTypeIdentifier: 'public.jwt_token',
+    enableCors: 'true'
+    }
+  )
+)
+
+app.listen(process.env.PORT || 5433)
