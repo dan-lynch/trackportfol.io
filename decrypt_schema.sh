bb0d118acb114a7c49f442f5ef1ae1b6b6bb6d8c
Daniel Lynch <dan@lynchy.co.nz>
2020-05-24 20:49:14 +1000

    fix decrypt script

--- decrypt_schema.sh
+++ decrypt_schema.sh
@@ -8,4 +8,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
 --output backend/db/init/02-functions-2.sql backend/db/init/02-functions-2.sql.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
---output backend/db/init/03-policies.sql backend/db/init/04-instruments.sql.gpg
+--output backend/db/init/03-policies.sql backend/db/init/03-policies.sql.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SCHEMA_PASSPHRASE" \
+--output backend/db/init/04-instruments.sql backend/db/init/04-instruments.sql.gpg
