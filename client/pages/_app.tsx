b7a58ee135c45a7f15190c84e5bddaa673428039
Dan Lynch <dan@danlynch.io>
2020-06-12 00:19:15 +1000

    new favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -25,7 +25,9 @@ class MyApp extends App {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Easily track your investment portfolio. We currently support over 8,000 US stocks. Bonds, Commodities, International Stocks and Crypto coming soon." />
-        <link rel="apple-touch-icon" href="./logo192.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="./manifest.json" />
       </Head>
       <ContextProvider>
