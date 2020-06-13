3fda5b86256b6e0c56f31682e13ca5cd351b1558
Dan Lynch <dan@danlynch.io>
2020-06-14 01:38:32 +1000

    fix typo

--- decrypt_protected_files.sh
+++ decrypt_protected_files.sh
@@ -1,8 +1,8 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output backend/db/init/00-database.sql backend/db/init/00-tables.sql.gpg
+--output backend/db/init/00-tables.sql backend/db/init/00-tables.sql.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output backend/db/init/01-functions.sql backend/db/init/01-functions-.sql.gpg
+--output backend/db/init/01-functions.sql backend/db/init/01-functions.sql.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
 --output backend/db/init/02-policies.sql backend/db/init/02-policies.sql.gpg
